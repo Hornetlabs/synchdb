cdb46750d049dfaac3016645c8a9e7a7e8ed3091
hanjianqiao <hanjianqiao@gmail.com>
2026-06-08 11:00:10 +0800

    clear connector state data in shared meory when calling synchdb_del_conninfo

--- src/backend/synchdb/synchdb.c
+++ src/backend/synchdb/synchdb.c
@@ -5028,11 +5028,18 @@ synchdb_get_state(PG_FUNCTION_ARGS)
 	funcctx = SRF_PERCALL_SETUP();
 	idx = (int *)funcctx->user_fctx;
 
-	while (*idx < count_active_connectors())
+	while (*idx < synchdb_max_connector_workers)
 	{
 		Datum values[7];
 		bool nulls[7] = {0};
 		HeapTuple tuple;
+
+		/* skip slots cleared by synchdb_del_conninfo */
+		if (sdb_state->connectors[*idx].conninfo.name[0] == '\0')
+		{
+				(*idx)++;
+				continue;
+		}
 
 		/* we only want to show the connectors created in current database */
 		if (strcasecmp(sdb_state->connectors[*idx].conninfo.dstdb,
@@ -5095,11 +5102,18 @@ synchdb_get_stats(PG_FUNCTION_ARGS)
 	funcctx = SRF_PERCALL_SETUP();
 	idx = (int *)funcctx->user_fctx;
 
-	while (*idx < count_active_connectors())
+	while (*idx < synchdb_max_connector_workers)
 	{
 		Datum values[20];
 		bool nulls[20] = {0};
 		HeapTuple tuple;
+
+		/* skip slots cleared by synchdb_del_conninfo */
+		if (sdb_state->connectors[*idx].conninfo.name[0] == '\0')
+		{
+				(*idx)++;
+				continue;
+		}
 
 		/* we only want to show the connectors created in current database */
 		if (strcasecmp(sdb_state->connectors[*idx].conninfo.dstdb,
@@ -6327,9 +6341,15 @@ synchdb_del_conninfo(PG_FUNCTION_ARGS)
 			elog(WARNING, "terminating dbz connector (%s) with pid %d. Shutdown timeout: %d ms",
 					NameStr(*name), (int)pid, DEBEZIUM_SHUTDOWN_TIMEOUT_MSEC);
 			DirectFunctionCall2(pg_terminate_backend, UInt32GetDatum(pid), Int64GetDatum(DEBEZIUM_SHUTDOWN_TIMEOUT_MSEC));
-			set_shm_connector_pid(connectorId, InvalidPid);
-
 		}
+
+		/* clear the shared memory slot so it no longer appears in state/stats views */
+		LWLockAcquire(&sdb_state->lock, LW_EXCLUSIVE);
+		memset(&sdb_state->connectors[connectorId], 0, sizeof(ActiveConnectors));
+		sdb_state->connectors[connectorId].pid   = InvalidPid;
+		sdb_state->connectors[connectorId].state = STATE_UNDEF;
+		sdb_state->connectors[connectorId].type  = TYPE_UNDEF;
+		LWLockRelease(&sdb_state->lock);
 	}
 
 	/* remove the connector info record */
