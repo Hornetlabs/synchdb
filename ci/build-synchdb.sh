5c72704f9ab36e04f98f9a4adda0279f4fa162ab
hanjianqiao <hanjianqiao@gmail.com>
2026-07-02 20:36:49 +0800

    fix typo

--- ci/build-synchdb.sh
+++ ci/build-synchdb.sh
@@ -81,7 +81,7 @@ function build_synchdb()
 
 	# TODO: currently build under IvorySQL 5.4 will failed: ExecTypeFromTL
 	# ---- mysql_fdw (FDW-based MySQL snapshot) ----
-	if [ "$TARGET_FLAVOR" != "ivorysql" ] || [ "$IVORYSQL_MAJOR" != 5]; then
+	if [ "$TARGET_FLAVOR" != "ivorysql" ] || [ "$IVORYSQL_MAJOR" != "5" ]; then
 		git clone https://github.com/EnterpriseDB/mysql_fdw.git --branch REL-2_9_3 "${srcdir}/contrib/mysql_fdw"
 		(
 			cd "${srcdir}/contrib/mysql_fdw"
