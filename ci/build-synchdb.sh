36a4f46edea4c8af96ae905ec89096ff330bdbff
hanjianqiao <hanjianqiao@gmail.com>
2026-07-02 15:39:42 +0800

    add build step for mysql_fdw

--- ci/build-synchdb.sh
+++ ci/build-synchdb.sh
@@ -79,6 +79,14 @@ function build_synchdb()
 		exit 1
 	fi
 
+	# ---- mysql_fdw (FDW-based MySQL snapshot) ----
+	git clone https://github.com/EnterpriseDB/mysql_fdw.git --branch REL-2_9_3
+	(
+		cd mysql_fdw
+		make PG_CONFIG="${pgconfig}"
+		make install PG_CONFIG="${pgconfig}"
+	)
+
 	# ---- protobuf-c (OLR connector dependency) ----
 	git clone https://github.com/protobuf-c/protobuf-c.git --branch v1.5.2
 	(
